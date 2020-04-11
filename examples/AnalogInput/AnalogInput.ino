adaade7819c1592588c32fa2e966327c13ba0f4a
Jonas Scharpf <jonas@brainelectronics.de>
2020-04-11 08:55:05 +0200

    read Ireg via python, added general helper functions

--- examples/AnalogInput/AnalogInput.ino
+++ examples/AnalogInput/AnalogInput.ino
@@ -1,13 +1,38 @@
 /*
-  Modbus-Arduino Example - Test Analog Input (Modbus IP ESP8266)
-  Read Analog sensor on Pin ADC (ADC input between 0 ... 1V)
-  Original library
-  Copyright by André Sarmento Barbosa
-  http://github.com/andresarmento/modbus-arduino
-
-  Current version
-  (c)2017 Alexander Emelianov (a.m.emelianov@gmail.com)
-  https://github.com/emelianov/modbus-esp8266
+ * @file HoldingReg.ino
+ *
+ * @author     brainelectronics <git@brainelectronics.de>
+ * @date       11 April 2020
+ * @version    0.1.0
+ *
+ * @brief      Read Input Register with value of ADC pin (0...1024)
+ *
+ * @required   readInputModbusIp.py at version 0.1.0
+ *
+ * The circuit:
+ *    ESP32 or ESP8266
+ *
+ * Note:
+ *  - None
+ *
+ * Logging:
+ *    0   Not set, output everything
+ *    10  Debug
+ *    20  Info
+ *    30  Warning
+ *    40  Error
+ *    50  Critical
+ *    255 Off
+ *
+ * History/Credits:
+ *  Original library
+ *  Copyright by André Sarmento Barbosa
+ *  http://github.com/andresarmento/modbus-arduino
+ *
+ *  Forked from
+ *  (c)2017 Alexander Emelianov (a.m.emelianov@gmail.com)
+ *  https://github.com/emelianov/modbus-esp8266
+ *
 */
 
 #ifdef ESP8266
@@ -17,48 +42,69 @@
 #endif
 #include <ModbusIP_ESP8266.h>
 
-//Modbus Registers Offsets
-const int SENSOR_IREG = 100;
+#include "general.h"
+#include "wifiConfig.h"
 
-//ModbusIP object
+// Modbus Registers Offsets
+const uint16_t SENSOR_IREG = 100;
+
+// ModbusIP object
 ModbusIP mb;
 
-long ts;
+uint32_t ts;
 
-void setup() {
- #ifdef ESP8266
-    Serial.begin(74880);
- #else
-    Serial.begin(115200);
- #endif
- 
-    WiFi.begin("your_ssid", "your_password");
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
+void setup()
+{
+  _debugOutputBuffer = (char *) calloc(255, sizeof(char));
+  _thisFileName = (char *) calloc(255, sizeof(char));
 
-    Serial.println("");
-    Serial.println("WiFi connected");  
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
+  DEBUG_BEGIN(DEBUG_BAUD_RATE);
 
-    mb.slave();		//Start Modbus IP
-    // Add SENSOR_IREG register - Use addIreg() for analog Inputs
-    mb.addIreg(SENSOR_IREG);
+  // IMPORTANT
+  // THE FOLLOWING LINES ARE MANDATORY FOR EACH SKETCH
 
-    ts = millis();
+  // get filename without extension from filePath
+  _thisFileName = getSketchFileName(__FILE__);
+
+  // display all available informations of this sketch on console
+  printSketchInfo((char*)_thisFileName);
+  // END OF MANDATORY STUFF
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    DEBUG_PRINT(".");
+  }
+
+  DEBUG_PRINTLN("");
+
+  sprintf(_debugOutputBuffer, "Connected with IP %s to %s", WiFi.localIP().toString().c_str(), WiFi.SSID().c_str());
+  printDebug(_debugOutputBuffer, LEVEL_INFO);
+
+  mb.slave();
+
+  // Add SENSOR_IREG register - Use addIreg() for analog Inputs
+  mb.addIreg(SENSOR_IREG);
+
+  ts = millis();
 }
 
-void loop() {
-   //Call once inside loop() - all magic here
-   mb.task();
+void loop()
+{
+  // call once inside loop() - all magic here
+  mb.task();
 
-   //Read each two seconds
-   if (millis() > ts + 2000) {
-       ts = millis();
-       //Setting raw value (0-1024)
-       mb.Ireg(SENSOR_IREG, analogRead(A0));
-   }
-   delay(10);
+  uint32_t nowTime = millis();
+
+  if (nowTime > ts + 2000)
+  {
+    ts = nowTime;
+
+    // set latest value to this Ireg (0-1024 10bit @ ESP8266, 12bit @ ESP32)
+    mb.Ireg(SENSOR_IREG, analogRead(A0));
+  }
+
+  delay(10);
 }
---
+++ examples/AnalogInput/readInputModbusIp.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# ----------------------------------------------------------------------------
+#
+#  @author       brainelectronics
+#  @file         readInputModbusIp.py
+#  @date         April, 2020
+#  @version      0.1.0
+#  @brief        Read input register via modbus on external IP
+#
+#  @required     AnalogInput.ino at version 0.1.0
+#  				 pymodbus 2.3.0 or higher
+#
+#  @usage        python3 readInputModbusIp.py
+#
+#  @todo         None
+# ----------------------------------------------------------------------------
+
+__author__ = "brainelectronics"
+__copyright__ = "Copyright by brainelectronics, ALL RIGHTS RESERVED"
+__credits__ = ["brainelectronics"]
+__version__ = "0.1.0"
+__maintainer__ = "brainelectronics"
+__email__ = "git@brainelectronics.de"
+__status__ = "Development"
+
+# pip install pymodbus
+from pymodbus.client.sync import ModbusTcpClient
+import time
+
+client = ModbusTcpClient(
+	host="192.168.178.61",
+	# retries=2,
+	# retry_on_empty=True,
+	port=502)
+
+# connect to the serial modbus server
+connection = client.connect()
+print("Connection result is %s" %(connection))
+
+# address: the starting address to read from
+registerAddress = 100
+# count:   the number of registers to read
+# theAmount = 1
+# unit:    the slave unit this request is targeting (the slave device ID)
+# slaveID = 12
+
+# function 04 - read input registers (address=0, count=1, unit=12)
+print("Reading data from register %s" %(registerAddress))
+data = client.read_input_registers(address=registerAddress)
+adcValue = 0
+if data != None:
+	# print(data)
+	# print(data.registers)
+	adcValue = int(data.registers[0])
+	print("%d" %(adcValue))
+time.sleep(1)
+
+client.close()
